0be200361ccb04f04f6bcf9f86723023ca5d2f7c
Gregory D Stula <gregstula.dev@icloud.com>
2017-02-15 12:49:23 -0500

    Implement athentication plug

---
+++ web/models/user.ex
@@ -0,0 +1,27 @@
+defmodule Rumbl.User do
+  use Rumbl.Web, :model
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put.change(changset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
+  end
+
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, ~w(name username), [])
+    |> validate_length(:username, min: 1, max: 20)
+  end
+end
