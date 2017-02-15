678c365ebcd9d973641feb43523fa6f99016d9ab
Gregory D Stula <gregstula.dev@icloud.com>
2017-02-15 18:11:12 -0500

    Define relationship between User and Video schema

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -3,7 +3,7 @@ defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
   alias Rumbl.User
 
-  plug :authenticate when action in [:index, :show]
+  plug :authenticate_user when action in [:index, :show]
 
   def index(conn, _params) do
     users = Repo.all(Rumbl.User)
@@ -33,14 +33,4 @@ defmodule Rumbl.UserController do
     render conn, "new.html", changeset: changeset
   end
 
-  defp authenticate(conn, _opts) do
-    if conn.assigns.current_user do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You must be logged in to access that page")
-      |> redirect(to: page_path(conn, :index))
-      |> halt()
-    end
-  end
 end
---
+++ web/models/video.ex
@@ -0,0 +1,23 @@
+defmodule Rumbl.Video do
+  use Rumbl.Web, :model
+
+  schema "videos" do
+    field :url, :string
+    field :title, :string
+    field :description, :string
+    belongs_to :user, Rumbl.User
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  @required_fields [:url, :title, :description]
+  @optional_fields []
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, @required_fields, @optional_fields)
+    |> validate_required(@required_fields)
+  end
+end
