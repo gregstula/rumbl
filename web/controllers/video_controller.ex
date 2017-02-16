1c3f67702bc93b6212372ac104fedc6f76c03844
Gregory D Stula <gregstula.dev@icloud.com>
2017-02-15 19:49:23 -0500

    Add category to video form

--- web/controllers/video_controller.ex
+++ web/controllers/video_controller.ex
@@ -1,5 +1,8 @@
 defmodule Rumbl.VideoController do
   use Rumbl.Web, :controller
+  alias Rumbl.Category
+
+  plug :load_categories when action in [:new, :create, :edit, :update]
 
   def action(conn, _) do
     apply(__MODULE__, action_name(conn), [conn, conn.params, conn.assigns.current_user])
@@ -75,4 +78,14 @@ defmodule Rumbl.VideoController do
   defp user_videos(user) do
     assoc(user, :videos)
   end
+
+  defp load_categories(conn, _opts) do
+    query =
+      Category
+      |> Category.alphabetical
+      |> Category.names_and_ids
+    categories = Repo.all query
+    assign(conn, :categories, categories)
+  end
 end
+
--- web/models/category.ex
+++ web/models/category.ex
@@ -15,4 +15,14 @@ defmodule Rumbl.Category do
     |> cast(params, [:name])
     |> validate_required([:name])
   end
+
+  # Returns a query
+  def alphabetical(query) do
+    from c in query, order_by: c.name
+  end
+
+  # Returns a query
+  def names_and_ids(query) do
+    from c in query, select: {c.name, c.id}
+  end
 end
