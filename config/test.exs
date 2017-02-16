60b2e093915bb98da7c899a2d4184216270c998e
Gregory D Stula <gregstula.dev@icloud.com>
2017-02-16 12:40:41 -0500

    Speed up test suit

--- config/test.exs
+++ config/test.exs
@@ -17,3 +17,7 @@ config :rumbl, Rumbl.Repo,
   database: "rumbl_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
+
+# Ease up on the number of hashing rounds to speed up test suite
+config :comeonin, :bcrypt_log_rounds, 4
+config :comeonin, :pbkdf2_rounds, 1
