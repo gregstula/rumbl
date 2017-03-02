314ae7f54bcfd977484832979d7401dc18a2611c
Gregory D Stula <gregstula.dev@icloud.com>
2017-03-02 15:53:45 -0500

    Annotations and Socket Tokens

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -1,12 +1,13 @@
 defmodule Rumbl.UserSocket do
   use Phoenix.Socket
 
-  ## Channels
-  # channel "room:*", Rumbl.RoomChannel
-
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
-  # transport :longpoll, Phoenix.Transports.LongPoll
+ 
+  ## Channels
+  channel "videos:*", Rumbl.VideoChannel
+
+ # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
@@ -19,9 +20,16 @@ defmodule Rumbl.UserSocket do
   #
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
-  def connect(_params, socket) do
-    {:ok, socket}
+  @max_age 2 * 7 * 24 * 60 * 60
+
+  def connect(%{"token" => token}, socket) do
+    case Phoenix.Token.verify(socket, "user socket", token, max_age: @max_age) do
+      {:ok, user_id} -> {:ok, assign(socket, :user_id, user_id)}
+      {:error, _reason} -> :error
+    end
   end
+  
+  def connect(_params, _socket), do: :error
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
   #
@@ -33,5 +41,6 @@ defmodule Rumbl.UserSocket do
   #     Rumbl.Endpoint.broadcast("users_socket:#{user.id}", "disconnect", %{})
   #
   # Returning `nil` makes this socket anonymous.
-  def id(_socket), do: nil
+  def id(socket), do: "users_socket:#{socket.assigns.user_id}"
+
 end
